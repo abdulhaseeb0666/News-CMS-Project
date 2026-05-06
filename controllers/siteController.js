2ca7fa9e5ff590bca49903f529fe92a93e86d53a
abdulhaseeb0666 <abdulhaseebah7886786@gmail.com>
2026-05-07 01:58:10 +0500

    On branch master Your branch is ahead of 'origin/master' by 1 commit.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   controllers/siteController.js
    	new file:   utils/paginate.js
    	modified:   views/author.ejs
    	modified:   views/category.ejs
    	modified:   views/index.ejs
    	modified:   views/layout.ejs
    	modified:   views/partials/sidebar.ejs
    	modified:   views/search.ejs

--- controllers/siteController.js
+++ controllers/siteController.js
@@ -4,60 +4,109 @@ import Category from "../models/Category.js";
 import News from "../models/News.js";
 import User from "../models/Users.js";
 import Comment from "../models/Comments.js";
+import Setting from "../models/Setting.js";
+
+import paginate from "../utils/paginate.js";
 
 export const index = async (req, res) => {
-    const articles = await News.find().populate("category" , {name : 1 , slug : 1}).populate("author" , "fullname").sort({createdAt : 1});
+    const setting = await Setting.findOne();
+
+    const recentAarticles = await News.find().populate("category" , {name : 1 , slug : 1}).populate("author" , "fullname").sort({createdAt : 1});
+    
+    const paginatedArticles = await paginate(News , {} , req.query , {
+        sort : "-createdAt",
+        populate : [
+            {path : "category" , select : "name slug"},
+            {path : "author" , select : "fullname"}
+        ]
+    }) 
+
     const categoriesInUse = await News.distinct("category");
     const categories = await Category.find({_id : {$in : categoriesInUse}});
     
-    res.render("index.ejs" , {articles , categories});
+    res.render("index.ejs" , {paginatedArticles , categories , setting , recentAarticles});
 };
 
 export const articleByCategory = async (req, res) => {
-    const articles = await News.find().populate("category" , {name : 1 , slug : 1}).populate("author" , "fullname").sort({createdAt : 1});
+    const setting = await Setting.findOne();
+
+    const recentAarticles = await News.find().populate("category" , {name : 1 , slug : 1}).populate("author" , "fullname").sort({createdAt : 1});
     const categoriesInUse = await News.distinct("category");
     const categories = await Category.find({_id : {$in : categoriesInUse}});
     
     const category = await Category.findOne({slug : req.params.name});
-    const categoryArticles = await News.find({category : category._id}).populate("category" , {name : 1 , slug : 1}).populate("author" , "fullname").sort({createdAt : 1});
     
-    res.render("category.ejs" , {categoryArticles , categories , category , articles});
+    const paginatedArticles = await paginate(News , {category : category._id} , req.query , {
+        sort : "-createdAt",
+        populate : [
+            {path : "category" , select : "name slug"},
+            {path : "author" , select : "fullname"}
+        ]
+    })
+
+    res.render("category.ejs" , {paginatedArticles , categories , category , recentAarticles , setting});
 };
 
 export const author = async (req, res) => {
-    const articles = await News.find().populate("category" , {name : 1 , slug : 1}).populate("author" , "fullname").sort({createdAt : 1});
+    const setting = await Setting.findOne();
+
+    const recentAarticles = await News.find().populate("category" , {name : 1 , slug : 1}).populate("author" , "fullname").sort({createdAt : 1});
     const categoriesInUse = await News.distinct("category");
     const categories = await Category.find({_id : {$in : categoriesInUse}});
     
     const author = await User.findOne({fullname : req.params.name});
     const authorArticles = await News.find({author : author.id}).populate("category" , {name : 1 , slug : 1}).populate("author" , "fullname").sort({createdAt : 1});
     
-    res.render("author.ejs" , {articles , categories , author , authorArticles});
+    const paginatedArticles = await paginate(News , {author : author.id} , req.query , {
+        sort : "-createdAt",
+        populate : [
+            {path : "category" , select : "name slug"},
+            {path : "author" , select : "fullname"}
+        ]
+    })
+
+    res.render("author.ejs" , {paginatedArticles , categories , author , recentAarticles , setting});
     
 };
 
 export const singleArticle = async (req, res) => {
-    const articles = await News.find().populate("category" , {name : 1 , slug : 1}).populate("author" , "fullname").sort({createdAt : 1});
+    const setting = await Setting.findOne();
+
+    const recentAarticles = await News.find().populate("category" , {name : 1 , slug : 1}).populate("author" , "fullname").sort({createdAt : 1});
     const categoriesInUse = await News.distinct("category");
     const categories = await Category.find({_id : {$in : categoriesInUse}});
     
     const article = await News.findById(req.params.id).populate("category" , {name : 1 , slug : 1}).populate("author" , "fullname").sort({createdAt : 1});
 
-    res.render("single.ejs" , {article , categories , articles});
+    res.render("single.ejs" , {recentAarticles , categories , article , setting});
     
 };
 
 export const search = async (req, res) => {
+    const setting = await Setting.findOne();
+    
     const search = req.query.search;
-
+    
+    const recentAarticles = await News.find().populate("category" , {name : 1 , slug : 1}).populate("author" , "fullname").sort({createdAt : 1});
     const articles = await News.find({$or : [
         {title : {$regex : search , $options : "i"}},
         {content : {$regex : search , $options : "i"}},
     ]}).populate("category" , {name : 1 , slug : 1}).populate("author" , "fullname").sort({createdAt : 1});
     const categoriesInUse = await News.distinct("category");
     const categories = await Category.find({_id : {$in : categoriesInUse}});
-    res.render("search.ejs" , {articles , categories , search});
     
+    const paginatedArticles = await paginate(News , {$or : [
+        {title : {$regex : search , $options : "i"}},
+        {content : {$regex : search , $options : "i"}},
+    ]} , req.query , {
+        sort : "-createdAt",
+        populate : [
+            {path : "category" , select : "name slug"},
+            {path : "author" , select : "fullname"}
+        ]
+    })
+    
+    res.render("search.ejs" , {recentAarticles , paginatedArticles ,  categories , search , setting});
 };
 
 export const addcomment = async (req, res) => {};
---
+++ utils/paginate.js
@@ -0,0 +1,35 @@
+const paginate = async (model , query = {} , reqQuery = {} , options = {}) => {
+    const {page = 1 , limit = 2 , sort = '-createdAt'} = reqQuery;
+
+    const paginationOptions = {
+        page : parseInt(page) ,
+        limit : parseInt(limit) ,
+        sort,
+        ...options
+    }
+
+    try{
+        const result = await model.paginate(query , paginationOptions)
+
+        return {
+            data: result.docs,
+            lastPage : result.lastPage,
+            nextPage: result.nextPage,
+            hasPrevPage: result.hasPrevPage,
+            hasNextPage: result.hasNextPage,
+            currentPage : result.page,
+            counter : result.pagingCounter,
+            limit: result.limit,
+            totalDocs: result.totalDocs,
+            totalPages: result.totalPages,
+            prevPage: result.prevPage,
+            nextPage: result.nextPage
+        }
+    }catch(err){
+        console.log("Pagination Error");
+
+    }
+}
+
+
+export default paginate;
